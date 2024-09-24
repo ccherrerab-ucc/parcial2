0d339e84b18c15e78c54dfc08a8d2a5d6b7fab67
ccherrerab <ccherrera913@ucatolica.edu.co>
2024-09-23 23:21:18 -0500

    SeguimientosSolicitudesRepository y Service

---
+++ src/main/java/com/GRH/myapp/GRH_myapp/repository/SeguimientosSolicitudesRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.GRH.myapp.GRH_myapp.repository;
+
+
+import com.GRH.myapp.GRH_myapp.model.SeguimientosSolicitudes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author cacri
+ */
+public interface SeguimientosSolicitudesRepository extends JpaRepository<SeguimientosSolicitudes, Integer> {
+    
+}
