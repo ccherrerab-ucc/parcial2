a3883bcdc67d1d6d7c0012183736a74ce20537ce
Luis Comezaquira <lecomezaquira83@ucatolica.edu.co>
2024-09-23 19:48:36 -0500

    creacion del repocitory y comienso del service

---
+++ src/main/java/com/GRH/myapp/GRH_myapp/repository/SeguimientoRepository.java
@@ -0,0 +1,6 @@
+package com.GRH.myapp.GRH_myapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SolicitudRepository extends JpaRepository<Solicitud, Long> {
+}
---
+++ src/main/java/com/GRH/myapp/GRH_myapp/service/SeguimientoService.java
@@ -0,0 +1,5 @@
+package com.GRH.myapp.GRH_myapp.service;
+
+public class SeguimientoService {
+
+}
