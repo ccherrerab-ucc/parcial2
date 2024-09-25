fbe08081deffd45627e6a77a0d4592833fc609f4
Luis Comezaquira <lecomezaquira83@ucatolica.edu.co>
2024-09-25 14:51:11 -0500

    rep request and status

--- src/main/java/com/GRH/myapp/GRH_myapp/repository/SeguimientoSolicitudesRepository.java
+++
@@ -1,6 +0,0 @@
-package com.GRH.myapp.GRH_myapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeguimientoSolicitudRepository extends JpaRepository<Solicitud, Long> {
-}
