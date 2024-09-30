aac7855f58cd30411eda5c10730c402c9aff69e7
Luis Comezaquira <lecomezaquira83@ucatolica.edu.co>
2024-09-30 16:52:24 -0500

    configuraciones finales IV

--- src/main/java/com/GRH/myapp/GRH_myapp/repository/SeguimientoSolicitudesRepository.java
+++ src/main/java/com/GRH/myapp/GRH_myapp/repository/SeguimientoSolicitudesRepository.java
@@ -1,7 +1,7 @@
 
 package com.GRH.myapp.GRH_myapp.repository;
 
-import com.GRH.myapp.GRH_myapp.model.SeguimientoSolicitudes;
+import com.GRH.myapp.GRH_myapp.model.SeguimientoSolicitude;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
